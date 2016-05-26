01481e9938982bd98422407ecaa6b63ddcd9e621
Angus Mezick <amezick@gmail.com>
2016-05-26 09:22:30 -0400

    update the format for the log

--- scripts/git/sendChangeEmail.sh
+++ scripts/git/sendChangeEmail.sh
@@ -3,7 +3,7 @@ cd [PROJECT_DIR]
 CURRENT_TAG=`git tag |tail -n 1`
 PREV_TAG=`git tag|tail -n 2 |head -n 1`
 
-git log $PREV_TAG..$CURRENT_TAG  >git.mail
+git log --pretty=format:"%B%n     %h %cn %ad%n     -------" $PREV_TAG..$CURRENT_TAG  >git.mail
 echo ' '>>git.mail
 echo ' '>>git.mail
 git diff $PREV_TAG $CURRENT_TAG --stat>>git.mail
